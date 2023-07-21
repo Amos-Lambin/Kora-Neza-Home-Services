e4eb2c677869c42a050ed91dff9987e9414929d1
Amos Lambin <amoslambin@gmail.com>
2023-07-21 22:10:21 +0200

    updated routes.dart
    added routeName for signupScreen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,11 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:kora_neza_home_services/screens/sign_up/sign_up_screen.dart';
 
 //We use name route
 //All our services will be available here
+
+final Map<String, WidgetBuilder> routes = {
+  SignUpScreen.routeName: (context) => const SignUpScreen(),
+};
