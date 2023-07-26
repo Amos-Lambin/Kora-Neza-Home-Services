5fa33ebe2f497f4543731d68e7e873232e0e745f
Amos Lambin <amoslambin@gmail.com>
2023-07-26 16:13:34 +0200

    Secondary Button

--- lib/components/secondary_btn.dart
+++ lib/components/secondary_btn.dart
@@ -1,1 +1,12 @@
 import 'package:flutter/material.dart';
+
+class SecondaryButton extends StatelessWidget {
+  const SecondaryButton({super.key, required this.text, required this.press});
+  final String text;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
