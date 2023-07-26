0c7e81791b7ddd66f772c94440b1128623c5c037
Amos Lambin <amoslambin@gmail.com>
2023-07-26 16:00:05 +0200

    Default Button

--- lib/components/default_button.dart
+++ lib/components/default_button.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
+import '../size_config.dart';
+
 class DefaultButton extends StatelessWidget {
-  const DefaultButton({super.key});
+  const DefaultButton({super.key, required this.text, required this.press});
+  final String text;
+  final Function press;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      width: double.infinity,
+      height: getProportionateScreenHeight(48),
+      child: ElevatedButton(
+        onPressed: () {
+          press();
+        },
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: getProportionateScreenHeight(18),
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
   }
 }
